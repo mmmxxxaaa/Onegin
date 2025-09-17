be213af4a895891b1f6057ba40df050826d576d5
mmmxxxaaa <maks.katkov.2007@mail.ru>
2025-09-17 19:47:30 +0300

    Delete work_with_pointers.h

--- work_with_pointers.h
+++
@@ -1,9 +0,0 @@
-#ifndef WORK_WITH_POINTERS_H_
-#define WORK_WITH_POINTERS_H_
-
-#include "io.h"
-
-void get_string_pointers(char* all_in_string, string_info* pointers_to_lines, long int amount_of_symbols, int max_lines);
-
-
-#endif // WORK_WITH_POINTERS_H_
