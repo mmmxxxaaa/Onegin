b37a9c3d37eb61df4b69cb06db8cdbc6ad8528bb
mmmxxxaaa <maks.katkov.2007@mail.ru>
2026-03-23 15:38:34 +0300

    Удалил ошибочный коммит

--- src/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-nasm -f elf64 -o printf.o printf.s
-g++ -fno-pie -c main.cpp -o main.o
-g++ -no-pie main.o printf.o -o program
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/my_io.h"
-
-int main()
-{
-    MyPrintf("check call from C %c, %o, %s\n", 'k', 101, "Vova Naumov bigbob");
-    return 0;
-}
